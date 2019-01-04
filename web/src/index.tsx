bf52b120d0b5c468fa8bad839ff80740a8fad88d
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-01-03 23:43:23 -0500

    Added the apollo gql queries | mutations combined with the tsx views

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,8 +1,20 @@
+import ApolloClient from "apollo-boost";
 import * as React from "react";
+import { ApolloProvider } from "react-apollo";
 import * as ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import { Routes } from "./Routes";
 
-ReactDOM.render(<Routes />, document.getElementById("root") as HTMLElement);
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  credentials: "include"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>,
+  document.getElementById("root") as HTMLElement
+);
 registerServiceWorker();
--- web/src/modules/user/MeView.tsx
+++ web/src/modules/user/MeView.tsx
@@ -1,7 +1,37 @@
+import { gql } from "apollo-boost";
 import * as React from "react";
+import { Query } from "react-apollo";
+import { MeQuery } from "src/schemaTypes";
+
+const meQuery = gql`
+  query MeQuery {
+    me {
+      id
+      email
+    }
+  }
+`;
 
 export class MeView extends React.PureComponent {
   render() {
-    return <div>me view</div>;
+    return (
+      <Query<MeQuery> query={meQuery}>
+        {({ data, loading }) => {
+          if (loading) {
+            return null;
+          }
+
+          if (!data) {
+            return <div>data is undefined</div>;
+          }
+
+          if (!data.me) {
+            return <div>received no user</div>;
+          }
+
+          return <div>{data.me.email}</div>;
+        }}
+      </Query>
+    );
   }
 }
